077d2ede46198e6782e40abd5c52280e2b84aab9
Hirofumi Seiyama <hseiyama@gmail.com>
2022-11-01 04:37:08 +0900

    udp通信を追加

--- test/iod_spi_w5100s/iod_spi_w5100s.c
+++ test/iod_spi_w5100s/iod_spi_w5100s.c
@@ -9,17 +9,19 @@
 #include "socket.h"
 
 #define PLL_SYS_KHZ     (133 * 1000) // Clock
-#define TCP_BUF_SIZE    (64) // Buffer
+#define BUFFER_SIZE     (64) // Buffer
 #define TCP_SN          (1) // Socket(0-3)
 #define TCP_PORT        (5000) // Port
+#define UDP_SN          (3) // Socket(0-3)
+#define UDP_PORT        (5002) // Port
 
 struct ether_data {
-    uint8_t au8_buffer[TCP_BUF_SIZE];
+    uint8_t au8_buffer[BUFFER_SIZE];
     int32_t s32_size;
     bool bl_flag;
 };
 
-static const wiz_NetInfo g_net_info =
+static const wiz_NetInfo csts_net_info =
     {
         .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
         .ip = {192, 168, 0, 150},                     // IP address
@@ -32,6 +34,11 @@ static const wiz_NetInfo g_net_info =
 static uint8_t u8s_tcp_sn;
 static struct ether_data sts_tcp_rx_data;
 static struct ether_data sts_tcp_tx_data;
+static uint8_t u8s_udp_sn;
+static uint8_t au8s_udp_ip[4];
+static uint16_t u16s_udp_port;
+static struct ether_data sts_udp_rx_data;
+static struct ether_data sts_udp_tx_data;
 
 // iod_spi_w5100s
 extern void iod_spi_w5100s_init();
@@ -42,16 +49,22 @@ extern void iod_spi_w5100s_main_in();
 extern void iod_spi_w5100s_main_out();
 extern uint32_t iod_spi_w5100s_tcp_recv(uint8_t *, uint32_t);
 extern uint32_t iod_spi_w5100s_tcp_send(uint8_t *, uint32_t);
+extern uint32_t iod_spi_w5100s_udp_recv(uint8_t *, uint32_t);
+extern uint32_t iod_spi_w5100s_udp_send(uint8_t *, uint32_t);
 
-static void set_clock_khz();
+static void iod_spi_w5100s_set_clock();
 static void iod_spi_w5100s_tcp_in();
 static void iod_spi_w5100s_tcp_out();
+static void iod_spi_w5100s_udp_in();
+static void iod_spi_w5100s_udp_out();
 static void iod_spi_w5100s_intr_gpio();
+static uint32_t iod_spi_w5100s_recv(uint8_t *, uint32_t, struct ether_data *);
+static uint32_t iod_spi_w5100s_send(uint8_t *, uint32_t, struct ether_data *);
 
 // 外部公開関数
 void main() {
-    uint8_t au8a_tcp_buffer[TCP_BUF_SIZE];
-    uint16_t u16a_tcp_size;
+    uint8_t au8a_buffer[BUFFER_SIZE];
+    uint16_t u16a_size;
     uint8_t u8a_count = 0;
 
 //    stdio_init_all();
@@ -60,12 +73,15 @@ void main() {
     while (true) {
         iod_spi_w5100s_main_in();
         {
-            u16a_tcp_size = iod_spi_w5100s_tcp_recv(au8a_tcp_buffer, sizeof(au8a_tcp_buffer));
-            if (u16a_tcp_size > 0) {
-                iod_spi_w5100s_tcp_send(au8a_tcp_buffer, u16a_tcp_size);
-            } else {
-                printf("passed. (%d)\n", u8a_count);
+            u16a_size = iod_spi_w5100s_tcp_recv(au8a_buffer, sizeof(au8a_buffer));
+            if (u16a_size > 0) {
+                iod_spi_w5100s_tcp_send(au8a_buffer, u16a_size);
             }
+            u16a_size = iod_spi_w5100s_udp_recv(au8a_buffer, sizeof(au8a_buffer));
+            if (u16a_size > 0) {
+                iod_spi_w5100s_udp_send(au8a_buffer, u16a_size);
+            }
+            printf("passed. (%d)\n", u8a_count);
         }
         iod_spi_w5100s_main_out();
         sleep_ms(1000);
@@ -77,22 +93,30 @@ void iod_spi_w5100s_init() {
     memset(&sts_tcp_rx_data, 0, sizeof(sts_tcp_rx_data));
     memset(&sts_tcp_tx_data, 0, sizeof(sts_tcp_tx_data));
     u8s_tcp_sn = 0;
+    memset(&sts_udp_rx_data, 0, sizeof(sts_udp_rx_data));
+    memset(&sts_udp_tx_data, 0, sizeof(sts_udp_tx_data));
+    memset(&au8s_udp_ip, 0, sizeof(au8s_udp_ip));
+    u8s_udp_sn = 0;
+    u16s_udp_port = 0;
 
     // Initialize
-    set_clock_khz();
+    iod_spi_w5100s_set_clock();
     wizchip_spi_initialize();
     wizchip_cris_initialize();
     wizchip_reset();
     wizchip_initialize();
     wizchip_check();
-    network_initialize(g_net_info);
+    network_initialize(csts_net_info);
     // 割り込み設定
+    //【注意】wizchip_gpio_interrupt_initialize() の仕様により、
+    //        IMR (Interrupt Mask Register)の設定は後勝ちとなる
     wizchip_gpio_interrupt_initialize(TCP_SN, iod_spi_w5100s_intr_gpio);
+//    wizchip_gpio_interrupt_initialize(UDP_SN, iod_spi_w5100s_intr_gpio);
 
     //【注意】stdio_init_all()はクロック設定後に実行する
     stdio_init_all();
     // Get network information
-    print_network_information(g_net_info);
+    print_network_information(csts_net_info);
     printf("start iod_spi_w5100s\n");
 }
 
@@ -107,32 +131,32 @@ void iod_spi_w5100s_main_1ms() {
 
 void iod_spi_w5100s_main_in() {
     iod_spi_w5100s_tcp_in();
+    iod_spi_w5100s_udp_in();
 }
 
 void iod_spi_w5100s_main_out() {
     iod_spi_w5100s_tcp_out();
+    iod_spi_w5100s_udp_out();
 }
 
 uint32_t iod_spi_w5100s_tcp_recv(uint8_t *pu8a_buffer, uint32_t u32a_size) {
-    uint32_t u32a_rcode = 0;
-    if (sts_tcp_rx_data.bl_flag) {
-        sts_tcp_rx_data.s32_size = MIN(sts_tcp_rx_data.s32_size, u32a_size);
-        memcpy(pu8a_buffer, sts_tcp_rx_data.au8_buffer, sts_tcp_rx_data.s32_size);
-        u32a_rcode = sts_tcp_rx_data.s32_size;
-    }
-    return u32a_rcode;
+    return iod_spi_w5100s_recv(pu8a_buffer, u32a_size, &sts_tcp_rx_data);
 }
 
 uint32_t iod_spi_w5100s_tcp_send(uint8_t *pu8a_buffer, uint32_t u32a_size) {
-    sts_tcp_tx_data.s32_size = MIN(TCP_BUF_SIZE, u32a_size);
-    memcpy(sts_tcp_tx_data.au8_buffer, pu8a_buffer, sts_tcp_tx_data.s32_size);
-    sts_tcp_tx_data.bl_flag = true;
+    return iod_spi_w5100s_send(pu8a_buffer, u32a_size, &sts_tcp_tx_data);
+}
 
-    return sts_tcp_tx_data.s32_size;
+uint32_t iod_spi_w5100s_udp_recv(uint8_t *pu8a_buffer, uint32_t u32a_size) {
+    return iod_spi_w5100s_recv(pu8a_buffer, u32a_size, &sts_udp_rx_data);
+}
+
+uint32_t iod_spi_w5100s_udp_send(uint8_t *pu8a_buffer, uint32_t u32a_size) {
+    return iod_spi_w5100s_send(pu8a_buffer, u32a_size, &sts_udp_tx_data);
 }
 
 // 内部関数
-static void set_clock_khz()
+static void iod_spi_w5100s_set_clock()
 {
     // set a system clock frequency in khz
     set_sys_clock_khz(PLL_SYS_KHZ, true);
@@ -188,16 +212,70 @@ static void iod_spi_w5100s_tcp_out() {
     }
 }
 
+static void iod_spi_w5100s_udp_in() {
+    sts_udp_rx_data.bl_flag = false;
+
+    switch(getSn_SR(u8s_udp_sn)) { // SOCKET n Status Register
+    case SOCK_CLOSED:
+        u8s_udp_sn = socket(UDP_SN, Sn_MR_UDP, UDP_PORT, 0x00);
+        break;
+    case SOCK_UDP :
+        if (getSn_RX_RSR(u8s_udp_sn) > 0) { // SOCKET n RX Received Size Register
+            sts_udp_rx_data.s32_size = recvfrom(u8s_udp_sn, sts_udp_rx_data.au8_buffer, sizeof(sts_udp_rx_data.au8_buffer), au8s_udp_ip, &u16s_udp_port);
+            if (sts_udp_rx_data.s32_size > 0) {
+                sts_udp_rx_data.bl_flag = true;
+            }
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+static void iod_spi_w5100s_udp_out() {
+    switch(getSn_SR(u8s_udp_sn)) { // SOCKET n Status Register
+    case SOCK_UDP :
+        if (sts_udp_tx_data.bl_flag) {
+            sendto(u8s_udp_sn, sts_udp_tx_data.au8_buffer, sts_udp_tx_data.s32_size, au8s_udp_ip, u16s_udp_port);
+            sts_udp_tx_data.bl_flag = false;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+static uint32_t iod_spi_w5100s_recv(uint8_t *pu8a_buffer, uint32_t u32a_size, struct ether_data *psta_data) {
+    uint32_t u32a_rcode = 0;
+    if (psta_data->bl_flag) {
+        psta_data->s32_size = MIN(psta_data->s32_size, u32a_size);
+        memcpy(pu8a_buffer, psta_data->au8_buffer, psta_data->s32_size);
+        u32a_rcode = psta_data->s32_size;
+    }
+    return u32a_rcode;
+}
+
+static uint32_t iod_spi_w5100s_send(uint8_t *pu8a_buffer, uint32_t u32a_size, struct ether_data *psta_data) {
+    psta_data->s32_size = MIN(BUFFER_SIZE, u32a_size);
+    memcpy(psta_data->au8_buffer, pu8a_buffer, psta_data->s32_size);
+    psta_data->bl_flag = true;
+
+    return psta_data->s32_size;
+}
+
 static void iod_spi_w5100s_intr_gpio() {
     if(getSn_IR(u8s_tcp_sn) & Sn_IR_CON) {
         setSn_IR(u8s_tcp_sn,Sn_IR_CON);
-        printf("CONNECTED Interrupt.\n");
+        printf("CONNECTED (tcp) Interrupt.\n");
     } else if (getSn_IR(u8s_tcp_sn) & Sn_IR_RECV) {
         setSn_IR(u8s_tcp_sn,Sn_IR_RECV);
-        printf("RECEIVED Interrupt.\n");
+        printf("RECEIVED (tcp) Interrupt.\n");
     } else if (getSn_IR(u8s_tcp_sn) & Sn_IR_DISCON) {
         setSn_IR(u8s_tcp_sn,Sn_IR_DISCON);
-        printf("DISCONNECTED Interrupt.\n");
+        printf("DISCONNECTED (tcp) Interrupt.\n");
+    } else if (getSn_IR(u8s_udp_sn) & Sn_IR_RECV) {
+        setSn_IR(u8s_udp_sn,Sn_IR_RECV);
+        printf("RECEIVED (udp) Interrupt.\n");
     } else {
         printf("Other Interrupt.\n");
     }
